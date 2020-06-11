cf84039d60dac5aca1092c298102a208b8cb2915
ingridpitta <ingridpitta056@gmail.com>
2020-06-11 20:00:27 -0300

    [bug-fix] updating dist-server and encrytp method

--- src/routes/public/routes.public.js
+++ src/routes/public/routes.public.js
@@ -4,7 +4,7 @@ import { AuthController } from "../../controllers";
 const router = Router();
 const authController = new AuthController();
 
-router.get("/auth/signup", authController.signup);
-router.get("/auth/login", authController.login);
+router.post("/auth/signup", authController.signup);
+router.post("/auth/login", authController.login);
 
 export default router;
