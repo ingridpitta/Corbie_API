d2361caf8dd8834dea5075c810ffa4be98b6accf
ingridpitta <ingridpitta056@gmail.com>
2020-06-30 00:47:41 -0300

    adding public folder content

---
+++ dist-server/public/precache-manifest.6f2e4a3bc403644c78ce33abeecb2b49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42899a66f333ece6107fcb4a8950dd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3eccdeb2099a7e8b5e",
+    "url": "/static/css/main.18c59e4d.chunk.css"
+  },
+  {
+    "revision": "2c6649daa2436a877a41",
+    "url": "/static/js/2.96d00f99.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.96d00f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3eccdeb2099a7e8b5e",
+    "url": "/static/js/main.b98f348d.chunk.js"
+  },
+  {
+    "revision": "c4bf2b1ba6e7b8bd3141",
+    "url": "/static/js/runtime-main.86ecafa1.js"
+  },
+  {
+    "revision": "4366896e97667dbe76af6dba5216a52f",
+    "url": "/static/media/Logo.4366896e.svg"
+  },
+  {
+    "revision": "8bcd8239c803f44c48971d64caddbd69",
+    "url": "/static/media/landingPage.8bcd8239.png"
+  }
+]);
